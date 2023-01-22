007684862d1a3f078b11a30bb09c70fc72c86856
Daniel Bustos <daniel.bustos.velez@udla.edu.ec>
2023-01-22 17:05:13 -0500

    Proyecto memotest con items reciclaje

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
+import Board from './components/Board/Board';
+const emojiList = ['🌲', '🌱','🧃', '♻️', '🔋','📦','📄','🗑️'];
 
-function App() {
+
+const App =()=>{
+  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
+  const [selectedMemoBlock, setselectedMemoBlock] = useState(null);
+  const [animating, setAnimating] = useState(false);
+  
+  useEffect( () => {
+    console.log(emojiList);
+    const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+    console.log(shuffledEmojiList);
+    setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i) => ({ index: i, emoji, flipped: false}) ));
+  }, []);
+
+  const shuffleArray = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+  const handleMemoClick = memoBlock => {
+    const flippedMemoBlock = { ...memoBlock, flipped: true };
+    let shuffledMemoBlocksCopy = [...shuffledMemoBlocks];
+    shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock);
+    setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+    if(selectedMemoBlock === null) {
+      setselectedMemoBlock(memoBlock);
+    } else if(selectedMemoBlock.emoji === memoBlock.emoji) {
+      setselectedMemoBlock(null);
+    } else {
+      setAnimating(true);
+      setTimeout(() => {
+        shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
+        shuffledMemoBlocksCopy.splice(selectedMemoBlock.index, 1, selectedMemoBlock);
+        setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+        setselectedMemoBlock(null);
+        setAnimating(false);
+      }, 1000);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='header'>Memotest</h1>
+      <p className='subtitle'>Une las parejas de elemnetos que intervienen en el reciclaje </p>
+
+      <Board memoBlocks={shuffledMemoBlocks} animating={animating}  handleMemoClick={handleMemoClick} />
+      <div>
+      <p className='subtitle'>Daniel Alexander Bustos Velez</p>
+      </div>
     </div>
+    
   );
 }
 
